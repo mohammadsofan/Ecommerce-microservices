dcd0df00c8d76c5f2afe0d9e7da2d17f0225a3aa
mohammadsofan <nassersofan@gmail.com>
2025-09-07 12:26:05 +0300

    feat: setup product service and add product, category models

---
+++ ProductService/src/ProductService.Domain/Models/Category.cs
@@ -0,0 +1,16 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace ProductService.Domain.Models
+{
+    public class Category
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public int Id { get; set; }
+        [BsonRequired]
+        public string Name { get; set; } = string.Empty;
+        [BsonRequired]
+        public string Description { get; set; } = string.Empty;
+    }
+}
---
+++ ProductService/src/ProductService.Domain/Models/Product.cs
@@ -0,0 +1,18 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace ProductService.Domain.Models{
+    public class Product{
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public int Id { get; set; }
+        [BsonRequired]
+        public string Name { get; set; } = string.Empty;
+        [BsonRequired]
+        public string Description { get; set; } = string.Empty;
+        [BsonRequired]
+        public decimal Price { get; set; }
+        [BsonRequired]
+        public int Stock { get; set; }
+    }
+}
