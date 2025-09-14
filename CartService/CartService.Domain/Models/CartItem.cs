24101886c832aeec186959dd2ab7c4d7ca016022
mohammadsofan <nassersofan@gmail.com>
2025-09-14 20:02:48 +0300

    feat: Add Cart, CartItem, and Products domain models and reference MongoDB.Bson in CartService.Domain

---
+++ CartService/CartService.Domain/Models/CartItem.cs
@@ -0,0 +1,9 @@
+﻿namespace CartService.Domain.Models
+{
+    public class CartItem
+    {
+        public string ProductId { get; set; } = string.Empty;
+        public int Quantity { get; set; }
+
+    }
+}
---
+++ CartService/CartService.Domain/Models/Products.cs
@@ -0,0 +1,15 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace CartService.Domain.Models
+{
+    public class Products
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; } = string.Empty;
+        [BsonRequired]
+        public decimal Price { get; set; }
+
+    }
+}
