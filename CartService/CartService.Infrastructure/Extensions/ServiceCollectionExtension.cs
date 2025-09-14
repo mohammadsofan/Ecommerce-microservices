d8bcc4c9c7e39fa134b3d0bc11c0acf86ef19334
mohammadsofan <nassersofan@gmail.com>
2025-09-14 20:16:44 +0300

    feat: add infrastructure DI, settings classes, and MassTransit/RabbitMQ/JWT/MongoDB configuration

---
+++ CartService/CartService.Infrastructure/Extensions/ServiceCollectionExtension.cs
@@ -0,0 +1,53 @@
+﻿using CartService.Infrastructure.Settings;
+using MassTransit;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using MongoDB.Driver;
+using System.Text;
+
+namespace CartService.Infrastructure.Extensions
+{
+    public static class ServiceCollectionExtension
+    {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.Configure<MongoDbSettings>(options =>
+            {
+                options.ConnectionString = configuration.GetSection("MongoDbSettings:ConnectionString").Value ?? "";
+                options.DatabaseName = configuration.GetSection("MongoDbSettings:DatabaseName").Value ?? "";
+            });
+            services.Configure<JwtSettings>(options =>
+            {
+                options.Key = configuration.GetSection("JWT:Key").Value ?? "";
+            });
+            services.AddSingleton<IMongoClient>(sp => new MongoClient(configuration.GetSection("MongoDbSettings:ConnectionString").Value));
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            }).AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters()
+                {
+                    ValidateAudience = false,
+                    ValidateIssuer = false,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration.GetSection("JWT:Key").Value!))
+                };
+            });
+            services.AddAuthorization();
+            services.AddMassTransit(config =>
+            {
+                config.UsingRabbitMq((ctx, cfg) =>
+                {
+                    var rabbitMqUri = new Uri(configuration.GetSection("RabbitMQ:Uri").Value ?? "amqp://localhost:5672");
+                    cfg.Host(rabbitMqUri);
+                });
+            });
+            return services;
+        }
+    }
+}
