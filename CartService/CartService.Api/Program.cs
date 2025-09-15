52cead1485e991c04271ae13555ce14caacf10af
mohammadsofan <nassersofan@gmail.com>
2025-09-15 12:03:03 +0300

    feat: update csproj with Serilog packages

--- CartService/CartService.Api/Program.cs
+++ CartService/CartService.Api/Program.cs
@@ -1,22 +1,42 @@
-var builder = WebApplication.CreateBuilder(args);
+using Serilog;
 
-// Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddControllers();
-builder.Services.AddOpenApi();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
+var config = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json")
+    .Build();
+Log.Logger = new LoggerConfiguration()
+    .ReadFrom.Configuration(config)
+    .CreateBootstrapLogger();
+var logger = Log.ForContext<Program>();
+logger.Information("Starting up ProductService Api");
+try
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-    app.MapOpenApi();
+    var builder = WebApplication.CreateBuilder(args);
+
+    // Add services to the container.
+    // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+    builder.Services.AddControllers();
+    builder.Services.AddOpenApi();
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
+    var app = builder.Build();
+
+    // Configure the HTTP request pipeline.
+    if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+        app.MapOpenApi();
+    }
+
+    app.UseHttpsRedirection();
+    app.MapControllers();
+    app.Run();
 }
-
-app.UseHttpsRedirection();
-app.MapControllers();
-app.Run();
-
+catch (Exception ex)
+{
+    logger.Fatal(ex, "An unhandled exception occurred during bootstrapping");
+}
+finally
+{
+    Log.CloseAndFlush();
+}
