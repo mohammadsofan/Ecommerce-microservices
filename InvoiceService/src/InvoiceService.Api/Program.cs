c12baaef3b2337dba8ed55420ebdf51085309a0d
sabriNoor <nooraosama63@gmail.com>
2025-09-18 19:49:18 +0300

    feat(program): set up application pipeline

--- InvoiceService/src/InvoiceService.Api/Program.cs
+++ InvoiceService/src/InvoiceService.Api/Program.cs
@@ -1,6 +1,28 @@
-var builder = WebApplication.CreateBuilder(args);
 
+using InvoiceService.Api.Middelwares;
+using InvoiceService.Infrastructure;
+using InvoiceService.Application;
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(new WebApplicationOptions
+{
+    Args = args,
+    EnvironmentName = Environments.Development
+});
+
+builder.Host.UseSerilog((context, configuration) =>
+    configuration.ReadFrom.Configuration(context.Configuration));
+
+// Add services to the container.
 builder.Services.AddOpenApi();
+builder.Services.AddAuthorization();
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+
+// DI for Infrastructure and Application layers
+builder.Services.AddInfrastructure(builder.Configuration);
+builder.Services.AddApplicationServices();
+
 
 var app = builder.Build();
 
@@ -9,9 +31,19 @@ if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
-
 app.UseHttpsRedirection();
+
+// Serilog Request Logging
+app.UseSerilogRequestLogging();
+
+// Global Exception Handling Middleware
+app.UseMiddleware<ExceptionHandlingMiddleware>();
+
+app.UseAuthentication();
+app.UseAuthorization();
+app.MapControllers();
 
 
 app.Run();
+
 
